41f3cd5e282a3314de8aafd8657fd8e6465b3a09
RinkuNahar <rinkunaharhossan2022@gmail.com>
2022-05-07 21:24:33 +0600

    jwt added

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -1,4 +1,5 @@
 
+import axios from 'axios';
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
@@ -29,16 +30,19 @@ const LogIn = () => {
 
     const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
 
-    const handleSubmit = event => {
+    const handleSubmit =async event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        const {data}=await axios.post('http://localhost:5000/login', {email});
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, { replace: true });
     }
     let from = location.state?.from?.pathname || "/";
 
     if (user) {
-        navigate(from, { replace: true });
+        // navigate(from, { replace: true });
     }
 
     if (error) {
